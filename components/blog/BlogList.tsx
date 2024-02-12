a0f049e576d3be95358c7e27d8558f7d9de76948
michael-belete <158163912+michael-belete@users.noreply.github.com>
2024-02-12 11:32:14 +0300

    Update BlogList.tsx

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -3,13 +3,11 @@ import BlogCard from "./BlogCard";
 
 const BlogList = ({ blogs }: { blogs: BlogNode[] }) => {
   return (
-    <section className="px-6 pt-32 md:px-8 md:pt-40">
-      <div className="max-w-7xl mx-auto gap-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {blogs.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </div>
-    </section>
+    <div className="flex flex-col gap-6">
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} blog={blog} />
+      ))}
+    </div>
   );
 };
 
