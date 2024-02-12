0cee6f02614d68c3b48096224338f964d73d9a93
michael-belete <158163912+michael-belete@users.noreply.github.com>
2024-02-12 17:29:34 +0300

    Update Logo.tsx

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import logo from "@/public/common/Linkt-logo.svg";
-import Linkt from "@/public/common/Linkt.svg";
+import logo from "@/public/common/linkt.svg";
 import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="flex gap-2 items-center">
-        <Image
-          className="object-cover w-9 h-9"
-          src={logo}
-          alt="Linkt logo icon"
-        />
-        <Image
-          className="object-cover w-12 h-6"
-          src={Linkt}
-          alt="Linkt logo text"
-        />
-      </div>
+      <Image
+        className="object-cover w-auto h-9 md:h-10"
+        src={logo}
+        alt="Linkt logo icon"
+        priority
+      />
     </Link>
   );
 };
