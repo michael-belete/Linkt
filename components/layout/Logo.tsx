1bc605c1bd33310fb5b17cc3222696b5cd972f08
michael-belete <158163912+michael-belete@users.noreply.github.com>
2024-02-12 17:32:47 +0300

    Update Logo.tsx

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import logo from "@/public/common/linkt.svg";
+import logo from "@/public/common/Linkt.svg";
 import Link from "next/link";
 const Logo = () => {
   return (
