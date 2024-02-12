5eeeeb30f1af26036640b56343504126c45dda6b
michael-belete <158163912+michael-belete@users.noreply.github.com>
2024-02-12 11:31:55 +0300

    Update BlogCard.tsx

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -7,43 +7,43 @@ import placeholder from "@/public/meta/cover.png";
 
 const BlogCard = ({ blog }: { blog: BlogNode }) => {
   return (
-    <Link href={`/blog/${blog.slug}`}>
-      <div className="max-w-7xl mx-auto flex flex-col rounded-xl p-6 gap-8 shadow-card bg-secondary duration-300 group hover:shadow-lg hover:-translate-y-2">
+    <Link href={`/blog/${blog.slug}`} prefetch={false}>
+      <div className="flex gap-6 duration-300 group">
         {blog.featuredImage ? (
           <Image
-            className="w-full h-48 border border-gray-800 object-cover rounded-xl"
+            className="border border-zinc-900 object-cover rounded-xl aspect-[16/9] shrink-0 h-fit w-36 md:w-full md:max-w-56"
             src={blog.featuredImage.node.mediaItemUrl}
             alt={blog.title}
-            width={800}
-            height={600}
+            width={533}
+            height={300}
           />
         ) : (
           <Image
-            className="w-full h-48 border border-gray-800 object-cover rounded-xl"
+            className="border border-zinc-900 object-cover rounded-xl aspect-[16/9] w-32 md:w-64"
             src={placeholder}
             alt="Linkt main blog placeholder image"
-            width={800}
-            height={600}
+            width={533}
+            height={300}
           />
         )}
 
-        <div className="flex flex-col gap-8">
-          <h2 className="text-lg font-bold duration-300 group-hover:text-primary">
+        <div className="flex flex-col gap-2 md:gap-4">
+          <h2 className="font-semibold duration-300 group-hover:text-primary line-clamp-3 lg:line-clamp-none md:text-lg">
             {blog.title}
           </h2>
 
           <div className="flex gap-2 items-center">
             <Image
-              className="object-cover rounded-full h-6 w-6"
+              className="object-cover rounded-full h-5 w-5"
               src={blog.author.node.avatar.url}
               alt={blog.author.node.name}
-              width={200}
-              height={200}
+              width={50}
+              height={50}
             />
-            <p className="text-sm">{blog.author.node.name}</p>
+            <p className="text-xs sm:text-sm">{blog.author.node.name}</p>
           </div>
 
-          <p className="text-sm text-silver-gray">
+          <p className="text-xs sm:text-sm text-silver-gray">
             {new Date(blog.date).toLocaleDateString("en-US", {
               year: "numeric",
               month: "long",
@@ -52,8 +52,10 @@ const BlogCard = ({ blog }: { blog: BlogNode }) => {
           </p>
 
           <div className="flex gap-2 items-center text-silver-gray">
-            <MdAccessTime className="w-6 h-6" />
-            <span className="text-sm">{blog.readingTime} min read</span>
+            <MdAccessTime className="w-4 h-4 sm:w-5 sm:h-5" />
+            <span className="text-xs sm:text-sm">
+              {blog.readingTime} min read
+            </span>
           </div>
         </div>
       </div>
