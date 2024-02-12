9e118ec913d119895de7f731b03539cb192e326b
michael-belete <158163912+michael-belete@users.noreply.github.com>
2024-02-12 11:33:55 +0300

    Update BlogDetailFallback.tsx

--- components/blog/fallback/BlogDetailFallback.tsx
+++ components/blog/fallback/BlogDetailFallback.tsx
@@ -1,43 +1,69 @@
+import { IoList } from "react-icons/io5";
+
 const BlogDetailFallback = () => {
   const fallbackAnimate = "bg-gray-800 animate-pulse";
-  const contentPlacholders = 4;
 
   return (
-    <div>
-      <section className="px-6 pt-20 md:px-8 md:pt-32">
-        <div className="mx-auto max-w-5xl flex flex-col gap-12">
-          <div className={`${fallbackAnimate} h-8 w-full rounded`} />
-
-          <div className="flex flex-col gap-4">
-            <div className="flex gap-2 items-center">
-              <div className={`${fallbackAnimate} rounded-full h-8 w-8`} />
-              <div className={`${fallbackAnimate} h-3 w-40 rounded`} />
+    <div className="flex flex-col gap-24 mx-auto max-w-7xl px-4 md:px-6 md:flex-row">
+      <div className="w-full flex flex-col gap-24">
+        <div>
+          <div className="flex flex-col gap-8 pb-8 border-b border-b-zinc-900 md:border-none md:pb-0">
+            <div className="flex flex-col gap-4">
+              <div
+                className={`md:w-2/3 w-full h-4 rounded ${fallbackAnimate}`}
+              />
+              <div
+                className={`md:w-1/2 w-2/3 h-4 rounded ${fallbackAnimate}`}
+              />
             </div>
 
-            <div className={`${fallbackAnimate} h-3 w-32 rounded`} />
+            <div className="flex flex-col gap-8 xs:items-center xs:flex-row">
+              <div className="flex gap-2 items-center">
+                <div className={`rounded-full h-6 w-6 ${fallbackAnimate}`} />
+                <div className={`w-16 h-3 rounded ${fallbackAnimate}`} />
+              </div>
 
-            <div className={`${fallbackAnimate} h-3 w-24 rounded`} />
+              <div className={`w-16 h-3 rounded ${fallbackAnimate}`} />
+
+              <div className={`w-16 h-3 rounded ${fallbackAnimate}`} />
+            </div>
+
+            <div
+              className={`w-full rounded-xl h-52 sm:h-[400px] ${fallbackAnimate}`}
+            />
           </div>
 
-          <div className={`${fallbackAnimate} w-full h-80 rounded-xl`} />
-        </div>
-      </section>
-
-      <section className="px-6 pt-20 md:px-8">
-        <div className="mx-auto max-w-5xl">
-          <div className="flex flex-col gap-4">
-            {Array.from(
-              { length: contentPlacholders },
-              (_, index) => index + 1
-            ).map((index) => (
-              <div
-                key={index}
-                className={`${fallbackAnimate} h-4 w-full rounded`}
-              />
-            ))}
+          <div className="flex-col gap-4 shrink-0 flex pt-8 md:hidden">
+            <div className="flex gap-2 items-center">
+              <IoList className="w-6 h-6" />
+              <p>On this page</p>
+            </div>
+            <div className="space-y-3">
+              <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+              <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+              <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+            </div>
           </div>
         </div>
-      </section>
+
+        <div className="flex flex-col gap-4">
+          <div className={`w-full h-3 rounded ${fallbackAnimate}`} />
+          <div className={`w-2/3 h-3 rounded ${fallbackAnimate}`} />
+          <div className={`w-1/2 h-3 rounded ${fallbackAnimate}`} />
+        </div>
+      </div>
+
+      <div className="flex-col gap-4 shrink-0 hidden md:flex">
+        <div className="flex gap-2 items-center">
+          <IoList className="w-6 h-6" />
+          <p>On this page</p>
+        </div>
+        <div className="space-y-3">
+          <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+          <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+          <div className={`w-24 h-3 rounded ${fallbackAnimate}`} />
+        </div>
+      </div>
     </div>
   );
 };
